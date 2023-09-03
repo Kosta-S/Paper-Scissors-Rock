c31f84b08c25366e375179daeb6c332da16b948c
Kosta Shlyak <constantineandi@gmail.com>
2023-09-03 15:07:31 +0300

    Made Paper, Scissors, Rock game fo r console

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+
+//function for computer selection
 let getComputerChoice =() =>{
     let possibleChoices = ['Paper', 'Scissors', 'Rock'];
     let compChoice =possibleChoices[Math.floor(Math.random() * 3)]
     return compChoice;
 }
+
+ //storing player selection
+ let playerSelection = () => {
+    let input = prompt("Type 's' for Scissors, 'r' for Rock, or 'p' for Paper");
+    if (input === 's' || input === 'p' || input === 'r') {
+      switch (input) {
+        case 's':
+          return 'Scissors';
+        case 'r':
+          return 'Rock';
+        case 'p':
+          return 'Paper';
+      }
+    } else {
+      alert('You pressed the wrong key!');
+    }
+  }
+  
+
+// function for playing one round
 let oneRoundOfGame = (playerSelection, computerSelection) => {
     if (playerSelection === "Rock" && computerSelection === "Rock") {
         return "It's a tie. Rock ties with rock.";
@@ -24,4 +46,30 @@ let oneRoundOfGame = (playerSelection, computerSelection) => {
         return "You win! Scissors beats paper.";
     }
 };
+
+let game = () =>{
+    let compScore = 0;
+    let playerScore= 0;
+    while (compScore <5 && playerScore <5){
+        let a=playerSelection();
+        let b =getComputerChoice();
+        let roundResult = oneRoundOfGame(a, b);
+        console.log(roundResult);
+
+        let playerWin = /win/i;
+        let compWin = /lose/i;
+        if (playerWin.test(roundResult)){
+            playerScore +=1;
+        } else if(compWin.test(roundResult)){
+            compScore +=1;
+        }
+        console.log(`Your score: ${playerScore}. Computer score: ${compScore}`);
+    }
+
+    compScore === 5? console.log('You\'ve  lost. Sorry!'): console.log("You\ve won! Congratulations!");
+
+ }
+
+    
+
 
